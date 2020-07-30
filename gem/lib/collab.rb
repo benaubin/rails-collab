48248de93dfa71f7c8a6e7633365f4353ee494e2
Ben Aubin <ben@benaubin.com>
2020-07-30 17:20:37 -0500

    Autoload Railtie

--- gem/lib/collab.rb
+++ gem/lib/collab.rb
@@ -1,5 +1,3 @@
-require "collab/railtie"
-
 module Collab
   def self.config
     @config ||= Collab::Config.new
@@ -9,6 +7,7 @@ module Collab
 
   autoload "Config", "collab/config"
   autoload "Bridge", "collab/bridge"
+  autoload "Railtie", "collab/railtie"
   autoload "HasCollaborativeDocument", "collab/has_collaborative_document"
   
   module Models
